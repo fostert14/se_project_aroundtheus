06e8db65215488b3d1c299d7d2dbe66455344ce4
Trevor Foster <trevafoser@gmail.com>
2023-06-19 22:34:03 -0500

    Make JavaScript Populate the Cards

--- scripts/index.js
+++ scripts/index.js
@@ -35,9 +35,29 @@ let profileName = document.querySelector(".profile__info-name");
 let profileDescription = document.querySelector(".profile__info-description");
 let nameInput = document.querySelector('input[name="name"]');
 let descriptionInput = document.querySelector('input[name="description"]');
+let cardSection = document.querySelector(".content");
+let cardTemplate =
+  document.querySelector("#card-template").content.firstElementChild;
 
 //Functions
 
+function getCardData(cardData) {
+  //clone the template element with all its content and store it in a cardElement variable
+  const cardElement = cardTemplate.cloneNode(true);
+  //access the card title and image and store them in variables
+  const cardImageTemplate = cardElement.querySelector(".content__card-image");
+  const cardTitleTemplate = cardElement.querySelector(".content__card-text");
+  //set the path to the image to the link field of the object
+  cardImageTemplate.src = cardData.link;
+  //set the image alt text to the name field of the object
+  cardImageTemplate.alt = cardData.name;
+  //set the card title to the name field of the object, too
+  cardTitleTemplate.textContent = cardData.name;
+  //return the ready HTML element with the filled-in data
+  return cardElement;
+}
+
+//change input of submitted form
 function handleProfileFormSubmit(evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
@@ -57,4 +77,9 @@ editButton.addEventListener("click", function () {
 
 exitButton.addEventListener("click", function () {
   elementModal.classList.remove("modal_opened");
+});
+
+initialCards.forEach((cardData) => {
+  const cardElement = getCardData(cardData);
+  cardSection.prepend(cardElement);
 });
